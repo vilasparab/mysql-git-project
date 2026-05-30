c46f905be515ef1ec9e4b6a3750f6017140e640c
Vilas Parab <parab.vilas@gmail.com>
2026-05-30 15:12:02 +0530

    Initial MySQL project

---
+++ college_lab.sql
@@ -0,0 +1,48 @@
+-----
+create database PracticeHYD
+
+use PracticeHYD
+
+-----
+CREATE TABLE Student(
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    course VARCHAR(30),
+    city VARCHAR(30),
+    marks INT
+);
+
+DESC Student;
+-----
+INSERT INTO Student
+VALUES
+(101,'Amit','BCA','Mumbai',85),
+(102,'Sneha','BSc','Pune',78),
+(103,'Rahul','BCom','Nashik',90);
+
+SELECT * FROM Student;
+
+-----
+ALTER TABLE Student
+ADD mobile_no VARCHAR(15);
+
+-----
+UPDATE Student
+SET marks = 95
+WHERE student_id = 101;
+
+SELECT * FROM Student;
+
+-----
+DELETE FROM Student
+WHERE student_id = 103;
+
+SELECT * FROM Student;
+
+-----
+TRUNCATE TABLE Student;
+SELECT * FROM Student;
+
+-----
+-- DROP TABLE Student;
+-- DROP DATABASE CollegeDB;
